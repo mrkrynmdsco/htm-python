e5a74b22ded51436f3e84fa8ba653eb32f0b2f2a
Mark Dasco <mrkrynmdsco@gmail.com>
2019-11-19 16:12:16 +0800

    Add init test SDR

--- htm/core/sdr.py
+++ htm/core/sdr.py
@@ -1,8 +1,41 @@
 
+import numpy as np
+
+
 class SDR(object):
     """
-        Sparse Distributed Representation
+    Sparse Distributed Representation
+
+    ...
+
+    Attributes
+    ----------
+    ncells : int
+        number of cells
+    sparsity : float
+        sparse percentage
+    wcells : int
+        number of active cells
+
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, ncells: int = 2048, sparsity: float = 0.02):
+        self._ncells = ncells
+        self._sparsity = sparsity
+        self._wcells = self._compute_population(ncells, sparsity)
+
+    @property
+    def ncells(self):
+        return self._ncells
+
+    @property
+    def sparsity(self):
+        return self._sparsity
+
+    @property
+    def wcells(self):
+        return self._wcells
+
+    @staticmethod
+    def _compute_population(n: int, s: float) -> int:
+        return int(n * s)
--- tests/core/sdr_test.py
+++ tests/core/sdr_test.py
@@ -3,16 +3,27 @@ import unittest as ut
 from htm.core.sdr import SDR
 
 
-class SDR_InitializationTest(ut.TestCase):
+DEF_NCELL = 2048
+DEF_SPARSE_PCT = 0.02
+DEF_ON_CELLS = 40
 
-    def setUp(self):
+
+class SDR_InitializationTests(ut.TestCase):
+
+    def test_default_cell_count(self):
         sdr = SDR()
+        self.assertEqual(sdr.ncells, DEF_NCELL)
+        self.assertIsInstance(sdr.ncells, int)
 
-    def test_dummy(self):
-        self.assertTrue(True)
+    def test_default_sparsity(self):
+        sdr = SDR()
+        self.assertEqual(sdr.sparsity, DEF_SPARSE_PCT)
+        self.assertIsInstance(sdr.sparsity, float)
 
-    def tearDown(self):
-        pass
+    def test_default_active_cells(self):
+        sdr = SDR()
+        self.assertEqual(sdr.wcells, DEF_ON_CELLS)
+        self.assertIsInstance(sdr.wcells, int)
 
 
 if __name__ == '__main__':
