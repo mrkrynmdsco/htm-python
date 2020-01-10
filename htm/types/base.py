34501b423366515414d509fd41a2a385e0a09b06
Mark Dasco <mrkrynmdsco@gmail.com>
2020-01-10 16:33:11 +0800

    Add connection methods

--- htm/types/base.py
+++ htm/types/base.py
@@ -56,23 +56,30 @@ class Connections (Map):
     def __init__(self, size: int):
         super().__init__(size=size)
         cfg = {
-            'max_conns': 16,    # maximum number of connections per map cell
-            'min_conns': 4,     # minimum number of connections per map cell
+            'max_conns_per_cell': 16,    # maximum number of connections per map cell
         }
         self.append_cfgs(cfg)
 
         self.initialize()
 
+    def get_connection(self, idx: int):
+        return self.map[idx]
+
     def add_connection(self, idx: int, to_idx: int):
-        if to_idx not in self.map[idx] and self.getcfg('max_conns') > len(self.map[idx]):
+        if to_idx not in self.map[idx] and self.getcfg('max_conns_per_cell') > len(self.map[idx]):
             self.map[idx].append(to_idx)
 
     def del_connection(self, idx: int, to_idx: int):
         if to_idx in self.map[idx]:
             self.map[idx].remove(to_idx)
 
-    def get_connections(self, idx: int):
-        return self.map[idx]
+    def add_connections(self, idx: int, ilst: list):
+        for i in ilst:
+            self.add_connection(idx, i)
+
+    def del_connections(self, idx: int, ilst: list):
+        for i in ilst:
+            self.del_connection(idx, i)
 
 
 class Memory (_htmObj_):
